55297cda964b2b6e30b3574859ce388842813902
Martin Askestad <MartinAskestad@users.noreply.github.com>
2022-02-26 09:25:34 +0100

    Add click handlers to buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'calc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calculator';
+  private _expression = '0';
+  public get expression() {
+    if (this._expression.startsWith('0') && this._expression.length > 1) {
+      return this._expression.substr(1);
+    }
+    return this._expression;
+  }
+  public set expression(value) {
+    this._expression = value;
+  }
+
+  onButtonClick(e: Event) {
+    e.preventDefault();
+    const target = e.target as HTMLButtonElement;
+    const text = target.innerText
+      .replace('·', '.')
+      .replace('×', '*')
+      .replace('÷', '/');
+    this.expression += text;
+  }
 }
